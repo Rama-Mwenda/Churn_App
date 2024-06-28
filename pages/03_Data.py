632393cb65befd19ee7b2a6d2a8fa22a1a518349
Ramadhan Mwenda <ngige.mwenda@outlook.com>
2024-06-28 20:53:16 +0300

    Rename data folder and update data page

--- pages/03_Data.py
+++ pages/03_Data.py
@@ -8,6 +8,9 @@ st.set_page_config(
     layout= 'wide'
 )
 
+st.title('Check the data used in building this Web app')
+st.write('Click the buttons to show the dataset')
+
 df = pd.read_csv('./data/Dataset.csv')
 
 #show numeric columns
